d887613be52332a68036cfcf12c811d8d2556bc6
mukund720 <mukund720@gmail.com>
2022-02-11 15:18:23 +0530

    2 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
+import { MatInputModule} from '@angular/material';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
@@ -10,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),MatInputModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
